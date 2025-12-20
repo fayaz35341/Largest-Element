44fe01a21cfc969723f029c58b26462009b1d2ec
Riyaz Shaik <94755295+fayaz35341@users.noreply.github.com>
2025-12-20 06:32:25 +0530

    Update Largest-Element.py

--- Largest-Element.py
+++ Largest-Element.py
@@ -1,9 +1,16 @@
+# class Solution:
+#     def largestElement(self, nums):
+#         L=nums[0]
+#         for i in range(len(nums)-1):
+#             if nums[i]<nums[i+1]:
+#                 L=nums[i+1]
+#         return L 
 class Solution:
     def largestElement(self, nums):
-        L=nums[0]
-        for i in range(len(nums)-1):
-            if nums[i]<nums[i+1]:
-                L=nums[i+1]
+        L=float('-inf')
+        for x in nums:
+            if x>L:
+                L=x
         return L 
 n=[-4, -3, 0, 1, -8]
 print(Solution().largestElement(n))
