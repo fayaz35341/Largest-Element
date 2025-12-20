fc864b093e52d7116af350a6e0b0ea4ed875fbd0
Riyaz Shaik <94755295+fayaz35341@users.noreply.github.com>
2025-12-20 06:33:01 +0530

    Update Largest-Element.js

--- Largest-Element.js
+++ Largest-Element.js
@@ -1,7 +1,18 @@
+// class Solution {
+//     largestElement(nums) {
+//         let L=nums[0]
+//         for (let i=1; i<nums.length-1;i++){
+//             if (nums[i]<nums[i+1]){
+//                 L=nums[i+1]
+//             }
+//         }
+//         return L
+//     }
+// }
 class Solution {
     largestElement(nums) {
         let L=nums[0]
-        for (let i=1; i<nums.length-1;i++){
+        for (let i=1; i<nums.length;i++){
             if (nums[i]<nums[i+1]){
                 L=nums[i+1]
             }
